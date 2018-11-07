30b0e1515600e48c5d05199ef4fa2f822f7f7296
bzsinger <benjamin.z.singer@gmail.com>
2018-11-06 18:21:15 -0600

    Add label to show instruction view

--- ShapeCreator/Walkthrough/WalkthroughView.m
+++ ShapeCreator/Walkthrough/WalkthroughView.m
@@ -11,9 +11,10 @@
 
 @interface WalkthroughView () {
     UIView *instructionView;
+    InstructionView *panGestureInstruction;
     InstructionView *singleTapInstruction;
     InstructionView *doubleTapInstruction;
-    InstructionView *panGestureInstruction;
+    UILabel *shakePhoneLabel;
 }
 
 @property (readwrite) UIButton *closeButton;
@@ -39,6 +40,10 @@
 
         doubleTapInstruction = [[InstructionView alloc] initWithFrame:CGRectZero image:[UIImage imageNamed:@"double_tap_gesture.png"] text:@"Delete"];
         [instructionView addSubview:doubleTapInstruction];
+
+        shakePhoneLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+        shakePhoneLabel.text = @"Shake phone to show this view";
+        [instructionView addSubview:shakePhoneLabel];
     }
     return self;
 }
@@ -47,7 +52,14 @@
     [super layoutSubviews];
     self.backgroundColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0];
 
-    CGRect instructionFrame = CGRectMake(CGRectGetWidth(self.frame) / 8, CGRectGetHeight(self.frame) / 8, CGRectGetWidth(self.frame) * 3 / 4, CGRectGetHeight(self.frame) * 3 / 4);
+    CGFloat frameWidth = CGRectGetWidth(self.frame);
+    CGFloat frameHeight = CGRectGetHeight(self.frame);
+    CGFloat frameDivisor = 1.0 / 8.0;
+    CGFloat dimensionMultiplier = 3.0 / 4.0;
+    CGRect instructionFrame = CGRectMake(frameWidth * frameDivisor,
+                                         frameHeight * frameDivisor,
+                                         frameWidth * dimensionMultiplier,
+                                         frameHeight * dimensionMultiplier);
 
     instructionView.frame = instructionFrame;
     instructionView.backgroundColor = [UIColor whiteColor];
@@ -62,9 +74,28 @@
                                         buttonSize.width,
                                         buttonSize.height);
 
-    panGestureInstruction.frame = CGRectMake(0, 20, CGRectGetWidth(instructionView.frame), 125);
-    singleTapInstruction.frame = CGRectMake(0, CGRectGetHeight(singleTapInstruction.frame) + 70, CGRectGetWidth(instructionView.frame), 125);
-    doubleTapInstruction.frame = CGRectMake(0, CGRectGetHeight(singleTapInstruction.frame) + CGRectGetHeight(doubleTapInstruction.frame) + 120, CGRectGetWidth(instructionView.frame), 125);
+    CGFloat topBuffer = 20.0;
+    CGFloat instructionWidth = CGRectGetWidth(instructionView.frame);
+    CGFloat instructionHeight = 125.0;
+    CGFloat fullInstructionHeight = 30.0 + instructionHeight;
+    panGestureInstruction.frame = CGRectMake(0,
+                                             topBuffer,
+                                             instructionWidth,
+                                             instructionHeight);
+    singleTapInstruction.frame = CGRectMake(0,
+                                            topBuffer + fullInstructionHeight,
+                                            instructionWidth,
+                                            instructionHeight);
+    doubleTapInstruction.frame = CGRectMake(0,
+                                            topBuffer + 2 * (fullInstructionHeight),
+                                            instructionWidth,
+                                            instructionHeight);
+
+    [shakePhoneLabel sizeToFit];
+    shakePhoneLabel.frame = CGRectMake(CGRectGetWidth(instructionView.frame) / 2 - CGRectGetWidth(shakePhoneLabel.frame) / 2,
+                                       topBuffer + 3 * (fullInstructionHeight),
+                                       CGRectGetWidth(shakePhoneLabel.frame),
+                                       CGRectGetHeight(shakePhoneLabel.frame));
 }
 
 @end
